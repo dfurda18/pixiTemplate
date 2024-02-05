0480355d07f1841ea45188e89fd09f4700eab2a2
Dario Urdapilleta <dario@charityrepublic.com>
2024-02-05 15:44:56 -0400

    Base Pixi Project

---
+++ src/scripts/game/Game.js
@@ -0,0 +1,16 @@
+import * as PIXI from "pixi.js";
+import { App } from "../system/App";
+import { Scene } from "../system/Scene";
+
+export class Game extends Scene {
+    create() {
+        this.createBackground();
+    }
+    createBackground() {
+        this.bg = App.sprite("bg");
+        this.bg.width = window.innerWidth;
+        this.bg.height = window.innerHeight;
+        this.container.addChild(this.bg);
+    }
+
+}
